19a7c6516e588295668b9ec884b24df249fe6ab8
[Nhi Huynh] <[nhi.huynh@hotmail.com]>
2025-07-24 18:52:43 +0800

    feat(teacher)!: added the teacher controller

--- models/student.py
+++ models/student.py
@@ -4,7 +4,7 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 class Student(db.Model):
     # Name of the table
     # __tablename__ = "students"
-    
+
     # Attributes
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
